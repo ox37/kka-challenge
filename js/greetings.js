b9ad0ab012fbdb2d3b7db2a2046d18f14c004329
ox37 <bhdox37@gmail.com>
2023-06-26 02:45:52 +0900

    fix login process

--- js/greetings.js
+++ js/greetings.js
@@ -3,11 +3,14 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const NONVISIBLE_CLASSNAME = "non-visible";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  console.log("test");
+  // loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginForm.classList.add(NONVISIBLE_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
@@ -23,5 +26,6 @@ if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
+  loginForm.classList.add(NONVISIBLE_CLASSNAME);
   paintGreetings(savedUsername);
 }
